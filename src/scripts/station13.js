de259860b95b3b0950a1c3798d50675f4685775e
kazuki1023 <k.keio12569@gmail.com>
2023-08-20 21:49:54 +0900

    finish station13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,10 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  text = document.getElementById("text");
+  if (e.target.checked) {
+    text.style.backgroundColor = "red";
+  }
+  else {
+    text.style.backgroundColor = "white";
+  }
 }
